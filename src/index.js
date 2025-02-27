3da0b3e39d1e0c3b464ee813ec473762cef7c1cb
alexwrobinson7 <56927762+alexwrobinson7@users.noreply.github.com>
2025-02-27 12:39:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
-import BuyerDashboard from './pages/BuyerDashboard.jsx'; 
+import BuyerDashboard from './pages/BuyerDashboard'; 
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import ProtectedRoute from './components/common/ProtectedRoute';
 
 function App() {
   const { user } = useAuth();
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={user ? <Navigate to="/dashboard.jsx" /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route 
-          path="/dashboard.jsx/*" 
+          path="/" 
+          element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} 
+        />
+        <Route 
+          path="/login" 
+          element={!user ? <Login /> : <Navigate to="/dashboard" />} 
+        />
+        <Route 
+          path="/register" 
+          element={!user ? <Register /> : <Navigate to="/dashboard" />} 
+        />
+        <Route 
+          path="/dashboard/*" 
           element={
             <ProtectedRoute>
               <BuyerDashboard />

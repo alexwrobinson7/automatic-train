6972b25741e16d4f0dc7685463f1d6e5849669ce
alexwrobinson7 <56927762+alexwrobinson7@users.noreply.github.com>
2025-02-27 15:38:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import BuyerDashboard from './pages/BuyerDashboard'; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BuyerDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
